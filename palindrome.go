eb5e8d10422d45898c4991dd0defb7f4d33dcdc8
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-05-30 20:48:54 +0700

    palindrome recursive function

--- palindrome.go
+++ palindrome.go
@@ -6,10 +6,14 @@ func main() {
 	palindrome := "abbaa"
 	isPalindrome(palindrome)
 
+	if isPalindromeRecursive(palindrome) == true{
+		fmt.Println("Is Palindrome")
+	}else{
+		fmt.Println("Is Not Palindrome")
+	}
+
+
 }
-
-
-
 func isPalindrome (a string){
 
 	for i := 0; i<len(a)/2; i++ {
@@ -23,4 +27,17 @@ func isPalindrome (a string){
 	}
 	fmt.Println("Is Palindrome")
 
+}
+
+func isPalindromeRecursive(text string) bool {
+
+	if len(text) <= 1{
+		return true
+	}else if len(text) == 2 && text[0] == text[1] {
+		return  true
+	}else if text[0] != text[len(text)-1] {
+		return false
+	}
+
+	return isPalindromeRecursive(text[1 : len(text)-1])
 }
