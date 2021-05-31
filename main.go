5a02f1a45ec56bff9f9f15190992f6c9599ced30
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-05-31 21:08:19 +0700

    create auth controlller login and register

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"AP/golang-api-jwt/config"
+	"AP/golang-api-jwt/controller"
+	"github.com/gin-gonic/gin"
+)
+var (
+	db = config.SetupDatabaseConnection()
+	authController = controller.NewAuthController()
+
+)
 func main()  {
-	
+	router := gin.Default()
+	authRoutes := router.Group("/api/auth")
+	{
+		authRoutes.POST("/login",authController.Login)
+		authRoutes.POST("/register",authController.Register)
+	}
+	router.Run()
 }
