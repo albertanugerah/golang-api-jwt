bdfa79650dfcb1b7ee74c707045878dd19ff9035
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-05-31 22:15:31 +0700

    create jwt auth middleware

---
+++ middleware/jwt-auth.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"AP/golang-api-jwt/helper"
+	"AP/golang-api-jwt/service"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+//AuthorizeJWT validates token user given
+func AuthorizeJWT(service service.JWTService) gin.HandlerFunc  {
+	return func(context *gin.Context) {
+		authHeader := context.GetHeader("Authorization")
+		if authHeader == ""{
+			response := helper.BuildErrorResponse("Failed to process request", "No Token Found",nil)
+			context.AbortWithStatusJSON(http.StatusBadRequest, response)
+		}
+		token, err := service.ValidateToken(authHeader)
+		if token.Valid{
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id] :",claims["user_id"])
+			log.Println("Claim[issuer]: ",claims["issuer"])
+
+		}else{
+			log.Println(err)
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(),nil)
+			context.AbortWithStatusJSON(http.StatusUnauthorized,response)
+		}
+
+	}
+
+}
